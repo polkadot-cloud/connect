073d228ead5338ed9cc18589fcef92785a391e8a
Ross Bulat <ross@jkrb.io>
2026-04-22 21:12:43 +0700

    fix license

--- packages/connect-proxies/src/index.ts
+++ packages/connect-proxies/src/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2026 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
+// Copyright 2026 @polkadot-cloud/connect authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
 // Constants
